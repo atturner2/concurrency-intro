37a14847a674927fc554b7b0974d7f71c3860ddf
Alex Turner <aturner@Alexs-Laptop.askey.com>
2023-06-26 10:38:21 -0500

    basic channel example with deadlock

--- main.go
+++ main.go
@@ -6,26 +6,26 @@ import (
 )
 
 func main() {
-
-	start := time.Now()
-
+	now := time.Now()
 	defer func() {
-		fmt.Println(time.Since(start))
+		fmt.Println(time.Since(now))
 	}()
 
-	evilNinjas := []string{"Tommy", "Johnny", "Bobby", "Andy"}
+	smokeSignal := make(chan bool)
+	evilNinja := "Tommy"
 
-	for _, evilNinja := range evilNinjas {
-		go attack(evilNinja) // adding go creates concurrency by creating seperate GoRoutines. We need channels to communicate between them
-	}
-
-	time.Sleep(time.Second * 2)
-
+	go attack(evilNinja, smokeSignal)
+	fmt.Println("here is the smoke signal: ", <-smokeSignal)
+	//smokeSignal <- false
+	//arrow to the left on the left hand side of the channel variable is to recieve the channel variable
+	//this smoke signal can only be used by one ninja at a time, so if line 19 ins't commented out you have deadlock.
+	//bufferred channels are the solution to this, on the next branch
 }
 
-func attack(target string) {
+func attack(target string, attacked chan bool) {
 	time.Sleep(time.Second)
 
 	fmt.Println("Throwing ninja stars at ", target)
+	attacked <- true
 
 }
