674ad95da02354116dbeb16d97b1a05e3f81155a
Alex Turner <aturner@Alexs-Laptop.local>
2023-06-26 10:59:30 -0500

    bufferred channel of specific size

--- main.go
+++ main.go
@@ -6,20 +6,12 @@ import (
 )
 
 func main() {
-	now := time.Now()
-	defer func() {
-		fmt.Println(time.Since(now))
-	}()
+	channel := make(chan string, 2)
+	channel <- "First Message"
+	channel <- "second Message"
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
 
-	smokeSignal := make(chan bool)
-	evilNinja := "Tommy"
-
-	go attack(evilNinja, smokeSignal)
-	fmt.Println("here is the smoke signal: ", <-smokeSignal)
-	//smokeSignal <- false
-	//arrow to the left on the left hand side of the channel variable is to recieve the channel variable
-	//this smoke signal can only be used by one ninja at a time, so if line 19 ins't commented out you have deadlock.
-	//bufferred channels are the solution to this, on the next branch
 }
 
 func attack(target string, attacked chan bool) {
