b6854cac72b09d94c70c314178f2f372d6fbec07
Alex Turner <aturner@Alexs-Laptop.local>
2023-08-07 09:58:53 -0400

    select

--- main.go
+++ main.go
@@ -1,47 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-type Shape interface {
-	Area() float64
-	Perimeter() float64
-}
-
-type Rectangle struct {
-	width, height float64
-}
-
-func (r Rectangle) Area() float64 {
-	return r.width * r.height
-}
-
-func (r Rectangle) Perimeter() float64 {
-	return (2 * r.width) + (2 * r.height)
-}
+import "fmt"
 
 func main() {
-	channel := make(chan string)
-	//numRounds := 3
-	go throwingNinjaStar(channel)
-	for {
-		message, open := <-channel
-		if !open {
-			break
-		}
+	ninja1, ninja2 := make(chan string), make(chan string)
+
+	go captainElect(ninja1, "Ninja 1")
+	go captainElect(ninja2, "Ninja 2")
+
+	select {
+	case message := <-ninja1:
+		fmt.Println(message)
+	case message := <-ninja2:
 		fmt.Println(message)
 	}
+
 }
 
-func throwingNinjaStar(channel chan string) {
-	rand.Seed(time.Now().UnixNano())
-	numRounds := 3
-	for i := 0; i < numRounds; i++ {
-		score := rand.Intn(10)
-		channel <- fmt.Sprint("You scored: ", score)
-	}
-	close(channel)
+func captainElect(ninja chan string, message string) {
+	ninja <- message
 }
